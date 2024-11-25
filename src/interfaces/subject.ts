bc07c8e6e9281577d4c0f4b38b4891624f31dee3
Andres Jurado anmijurane <anmijurane@gmail.com>
2024-11-25 09:36:13 -0600

    [ADD] subject slice

---
+++ src/interfaces/subject.ts
@@ -0,0 +1,19 @@
+export interface Subject {
+  id: string;
+  name: string;
+  description: string;
+  status: string;
+}
+
+export interface SubjectState {
+  items: Subject[];
+  length: number;
+  current: Subject | object;
+}
+
+export interface SubjectActions {
+  getSubjects: () => Promise<void>;
+  setCurrentSubject: (subjectId: string) => void;
+}
+
+export type SubjectSlice = SubjectState & SubjectActions;
---
+++ src/store/subject.slice.ts
@@ -0,0 +1,25 @@
+import {create} from "zustand";
+import {Subject, SubjectSlice, SubjectState} from "../interfaces/subject.ts";
+import {HTTP} from "../Http/AxiosInstance.ts";
+
+const initialStateSubject:SubjectState = {
+  items: [],
+  length: 0,
+  current: {},
+}
+
+export const useSubjectsStore = create<SubjectSlice>((set, get) => ({
+  ...initialStateSubject,
+  getSubjects: async () => {
+    const response = await HTTP.GET<Subject[]>(`api/subjects`)
+    set({
+      items: response.data,
+      length: response.data.length,
+    })
+  },
+  setCurrentSubject: (id: string) => {
+    const state = get();
+    const current = state.items.find((item) => item.id === id);
+    set({ current: current });
+  },
+}));
