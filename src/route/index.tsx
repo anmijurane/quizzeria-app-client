44a532e5683dc13b08e0bc34b1cadf29687264c0
Andres Jurado anmijurane <anmijurane@gmail.com>
2024-11-24 23:58:53 -0600

    [ADD] browser router

---
+++ src/route/index.tsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter, type RouteObject } from "react-router-dom";
+import { HomePage, Questionary, AnswerQuestionary } from "../pages";
+
+const routes: RouteObject[] = [
+    {
+        path: "/",
+        Component: HomePage,
+        errorElement: "Handle Error",
+    },
+    {
+        path: '/questionary',
+        Component: Questionary,
+        errorElement: "Handle Error",
+    },
+    {
+        path: '/question/:id',
+        Component: AnswerQuestionary,
+        errorElement: "Handle Error",
+    }
+];
+
+export const browserRouter = createBrowserRouter(routes);
