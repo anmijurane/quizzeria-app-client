e051241d00f424ff9d57115e8916f76a03350900
M. Andres Jurado <anmijurane@gmail.com>
2024-12-10 23:04:32 -0600

    [MOD]: Initial page, remove poc store

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,61 +1,48 @@
 import { useEffect } from "react";
-import {useQuestionaryStore, useSessionStore, useSubjectsStore} from "../store";
+import { useSessionStore, useSubjectsStore } from "../store";
+import { ProtectedLayout } from "@src/layouts/Protected";
 
 export const HomePage = () => {
 
   const actionsSession = useSessionStore();
-  const actionsSubject = useSubjectsStore();
-  const actionsQuestionary = useQuestionaryStore()
+  const subjectItems = useSubjectsStore(state => state.items);
+  const getSubjects = useSubjectsStore(state => state.getSubjects);
+  // const actionsQuestionary = useQuestionaryStore()
 
   useEffect(() => {
-    console.log({ actionsSession, actionsSubject })
-  })
+    console.log({ actionsSession, subjectItems })
+  });
 
-  const handleSetSubject = async (subjectId: string) => {
-    actionsSubject.setCurrentSubject(subjectId);
-    await actionsQuestionary.getQuestions(subjectId);
-  }
+  // const handleSetSubject = async (subjectId: string) => {
+  //   actionsSubject.setCurrentSubject(subjectId);
+  //   await actionsQuestionary.getQuestions(subjectId);
+  // }
+
+  useEffect(() => {
+    const init = async () => {
+      await getSubjects();
+    }
+    init();
+  }, [getSubjects])
 
   return (
-    <div>
-      <button onClick={() => actionsSession.login('anmijurane1@gmail.com', 'Rn@i8Adf2')}>Login</button>
-      <button onClick={() => actionsSession.logout()}>Logout</button>
-      <hr/>
-
-      <button onClick={() => actionsSubject.getSubjects()}>getSubjects</button>
-
-      <hr/>
-      {
-        actionsSubject.items.map((subject) =>
-          <button
-            key={subject.id}
-            onClick={() => handleSetSubject(subject.id)}
-          >
-            {subject.name}
-          </button>
-        )
-      }
-      <hr/>
-      {
-        actionsQuestionary.items.map((question) => {
-          return (
-            <>
-              <ul key={question.id}>
-                <li>{question.text}</li>
-                <button
-                  onClick={() => actionsQuestionary.getAnswers(question.id)}
-                >Ver respuestas
-                </button>
-              </ul>
-              {question?.answers && question.answers.length > 0 && question.answers.map((answer) => {
-                return (
-                  <button onClick={() => actionsQuestionary.setAnswerSelected(question.id, answer.id)}>{answer.text}</button>
-                )
-              })}
-            </>
-          )
-        })
-      }
-    </div>
+    <ProtectedLayout>
+      <div className="bg-white shadow">
+        <div className="mx-auto max-v-7xl px-4 py-6 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold tracking-tight text-gray-900">Elije un questionario</h1>
+        </div>
+      </div>
+      <div className="bg-white shadow">
+        <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+          <div className="mt-6 my-10 lg:grid lg:grid-cols-2 lg:gap-x-6 lg:space-y-1">
+            {subjectItems.map((subject) => (
+              <div className="group relative">
+                <h2 className="text-2xl text-gray-700">{subject.name}</h2>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </ProtectedLayout>
   );
 };
