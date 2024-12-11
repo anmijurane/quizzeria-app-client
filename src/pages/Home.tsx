43381b512966168c9a93743dacb05df6fa3d17ee
M. Andres Jurado <anmijurane@gmail.com>
2024-12-11 17:50:23 -0600

    [MOD]: create structure card for subjects elements

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,18 @@
 import { useEffect } from "react";
 import { useSessionStore, useSubjectsStore } from "../store";
 import { ProtectedLayout } from "@src/layouts/Protected";
+import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "@ui/card";
+import { Badge } from "@ui/badge";
+import { Button } from "@src/components/ui/button";
+import { useNavigate } from "react-router-dom";
+import { Subject } from "@interfaces/subject";
 
 export const HomePage = () => {
 
+  const navigate = useNavigate();
   const actionsSession = useSessionStore();
   const subjectItems = useSubjectsStore(state => state.items);
+  const setCurrentSubject = useSubjectsStore(state => state.setCurrentSubject);
   const getSubjects = useSubjectsStore(state => state.getSubjects);
   // const actionsQuestionary = useQuestionaryStore()
 
@@ -23,7 +30,12 @@ export const HomePage = () => {
       await getSubjects();
     }
     init();
-  }, [getSubjects])
+  }, [getSubjects]);
+
+  const initialQuestionary = (subject: Pick<Subject, 'id'>) => {
+    setCurrentSubject(subject.id);
+    navigate(`/questionary/${subject.id}`);
+  }
 
   return (
     <ProtectedLayout>
@@ -34,11 +46,29 @@ export const HomePage = () => {
       </div>
       <div className="bg-white shadow">
         <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-          <div className="mt-6 my-10 lg:grid lg:grid-cols-2 lg:gap-x-6 lg:space-y-1">
+          <div className="mt-6 my-10 lg:grid lg:grid-cols-2 lg:gap-x-6">
             {subjectItems.map((subject) => (
-              <div className="group relative">
-                <h2 className="text-2xl text-gray-700">{subject.name}</h2>
-              </div>
+              <Card key={subject.id} className="m-4">
+                <CardHeader>
+                  <CardTitle>
+                    <h2 className="text-2xl font-normal text-black">
+                      {subject.name}
+                    </h2>
+                  </CardTitle>
+                  <CardDescription>
+                    <Badge variant='destructive'>{subject.status}</Badge>
+                  </CardDescription>
+                </CardHeader>
+                <CardFooter className="flex justify-end bg-gray-700 h-3">
+                  <Button
+                    variant='link'
+                    onClick={() => initialQuestionary(subject)}
+                    className="text-white text-1xl uppercase"
+                  >
+                    Iniciar
+                  </Button>
+                </CardFooter>
+              </Card>
             ))}
           </div>
         </div>
