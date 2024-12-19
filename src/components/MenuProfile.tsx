15c94cc0db66d0aabff69e37210bb7420b6ee60a
Andres Jurado anmijurane <anmijurane@gmail.com>
2024-12-19 10:10:24 -0600

    [MOD]: create MenuProfile and implementation on Navbar

---
+++ src/components/MenuProfile.tsx
@@ -0,0 +1,47 @@
+import {useMemo} from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@ui/dropdown-menu";
+import {Avatar, AvatarFallback} from "@ui/avatar.tsx";
+import {useSessionStore} from "@src/store";
+
+export const MenuProfile = () => {
+  const logout = useSessionStore(state => state.logout);
+  const name = useSessionStore(state => state.name);
+
+  const initialsOfTheName = useMemo(() => {
+    const splitName = name.split(' ');
+    const initialName = splitName[0][0];
+    return splitName.length <= 1 ? initialName : initialName + splitName[1][0];
+  }, [name]);
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Avatar className="hover:cursor-pointer">
+          <AvatarFallback>{initialsOfTheName}</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuLabel>{name}</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem onClick={() => console.log('Editar perfil')}>
+            Editar perfil
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuGroup>
+          <DropdownMenuItem onClick={() => logout()}>
+            Cerrar session
+          </DropdownMenuItem>
+          </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,23 @@
-import { FC, PropsWithChildren } from 'react';
+import type { FC, MouseEventHandler, PropsWithChildren } from 'react';
 
 import { useToggleWithValue } from '@hooks/useToggle';
-import { navbarItems, profileMenuActions } from '@src/utils/menu-actions';
+import { navbarItems } from '@src/utils/menu-actions';
 import { Tabs } from './Tabs';
+import {useSessionStore} from "@src/store";
+import {MenuProfile} from "@components/MenuProfile.tsx";
+import {Button} from "@ui/button.tsx";
 
 type Props = PropsWithChildren
 
 export const Navbar: FC<Props> = () => {
 
-  const [profileMenu, toggleProfileMenu] = useToggleWithValue<string, React.MouseEventHandler<HTMLButtonElement>>('hidden', 'absolute');
-  const [menuAppMobile, toggleMenuAppMobile] = useToggleWithValue<string, React.MouseEventHandler<HTMLButtonElement>>('hidden', 'block');
+  const loginSession = useSessionStore(state => state.login);
+  const isActiveSession = useSessionStore(state => state.isActiveSession)
+  const [menuAppMobile, toggleMenuAppMobile] = useToggleWithValue<string, MouseEventHandler<HTMLButtonElement>>('hidden', 'block');
+
+  const loginAction = async () => {
+    await loginSession('anmijurane1@gmail.com','Rn@i8Adf2');
+  }
 
   return (
     <nav className="bg-gray-800">
@@ -31,10 +39,6 @@ export const Navbar: FC<Props> = () => {
             </button>
           </div>
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-            {/* Image Logo */}
-            {/* <div className="flex shrink-0 items-center">
-              <img className="h-8 w-auto" src="" alt="Logo app" />
-            </div> */}
             <div className="hidden sm:ml-6 sm:block">
               <div className="flex space-x-4">
                 {<Tabs
@@ -42,37 +46,16 @@ export const Navbar: FC<Props> = () => {
                   typeComponent='NavLinkRRD'
                   classList={{
                     default: 'rounded-md px-3 py-2 text-sm font-medium',
-                    whenisActive: 'text-white bg-gray-900',
-                    whenIsntActive: 'text-gray-300 hover:bg-gray-700 hover:text-white'
+                    whenIsActive: 'text-white bg-gray-900',
+                    whenNotActive: 'text-gray-300 hover:bg-gray-700 hover:text-white'
                   }}
                 />}
-                {/* <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-white bg-gray-900" aria-current="page">Dashboard</a>
-                    <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Team</a> */}
               </div>
             </div>
           </div>
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
             <div className="relative ml-3">
-              <div>
-                <button onClick={toggleProfileMenu} type="button" className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                  <span className="absolute -inset-1.5"></span>
-                  <span className="sr-only">Open user menu</span>
-                  {/* image profile */}
-                  {/* TODO: create a component with name */}
-                  <img className="size-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-                </button>
-              </div>
-
-              <div className={`${profileMenu} right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 focus:outline-none`} role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabIndex={-1}>
-                <Tabs
-                  items={profileMenuActions}
-                  typeComponent='LinkRRD'
-                  classList={{ default: 'block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200' }}
-                />
-                {/* <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex={-1} id="user-menu-item-0">Your Profile</a>
-                <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex={-1} id="user-menu-item-1">Settings</a>
-                <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex={-1} id="user-menu-item-2">Sign out</a> */}
-              </div>
+              {isActiveSession ? <MenuProfile /> : <Button variant="secondary" onClick={loginAction}>Iniciar Sesion</Button>}
             </div>
           </div>
         </div>
@@ -85,8 +68,8 @@ export const Navbar: FC<Props> = () => {
             typeComponent='NavLinkRRD'
             classList={{
               default: 'block rounded-md px-3 py-2 text-base font-medium',
-              whenisActive: 'text-white bg-gray-900',
-              whenIsntActive: 'text-gray-300 hover:bg-gray-700 hover:text-white'
+              whenIsActive: 'text-white bg-gray-900',
+              whenNotActive: 'text-gray-300 hover:bg-gray-700 hover:text-white'
             }}
           />}
         </div>
